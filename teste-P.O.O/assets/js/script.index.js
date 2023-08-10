859ce5c68f8735c3ec35a45876cd539416ac401c
João Pedro Rocha <jpcrocha8@gmail.com>
2023-08-10 08:13:51 -0300

    new folder teste P.O.O

---
+++ teste-P.O.O/assets/js/script.index.js
@@ -0,0 +1,13 @@
+let personSimple = "Jhon Doe";
+
+const personObject = {
+    firstName:"Jhon",
+    lastName:"Doe",
+    age:50,
+    eyeColor:"blue"
+};
+
+personObject.age = 10;
+
+console.log(personSimple);
+console.log(personObject);
---
+++ to-do-list-P.O.O/assets/js/script.index.js
@@ -0,0 +1,13 @@
+let personSimple = "Jhon Doe";
+
+
+const personObject = new Object();
+
+personObject.firstName = "Jhon";
+personObject.LastName = "Doe";
+personObject.age = 50;
+personObject.EyeColor = "Blue";
+
+
+console.log(personSimple);
+console.log(personObject);
