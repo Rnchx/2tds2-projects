0a7b63a71b8015dda97bd44f435b7ed161eeeda5
João Pedro Rocha <jpcrocha8@gmail.com>
2023-08-17 10:17:47 -0300

    feat: create product class

--- criacao-de-categorias/script.js
+++ criacao-de-categorias/script.js
@@ -6,6 +6,15 @@ class Category {
     }
 }
 
+class Product {
+    constructor (id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
 
 
 
