72bfaa87cf336781def4fdce470bd42acdca02dc
João Pedro Rocha <jpcrocha8@gmail.com>
2023-08-17 17:23:45 -0300

    criação de categorias

--- criacao-de-categorias/script.js
+++ criacao-de-categorias/script.js
@@ -37,17 +37,55 @@ class ProductServices {
     addProduct(name, price, category) {
         const id = this.nextProductId ++;
         const product = new Product(id, name, price, category);
-        category.product.push(product);
+        category.products.push(product);
         this.products.push(product);
     }
 }
 
 const categoryList = new CategoryService();
+const productList = new ProductServices();
 
 function createCategory() {
-    const categoryName = "Doce";
+    const categoryName = document.getElementById("categoryName").value;
 
     categoryList.addCategory(categoryName);
 
-    console.log(categoryList.categories);
+    document.getElementById("categoryName").value = "";
+}
+
+function createProduct() {
+    const productName = document.getElementById("name-product").value;
+    const price = document.getElementById("price-product").value;
+    const category = categoryList.categories[0];
+    let newProduct;
+
+    productList.addProduct(productName, price, category);
+
+    cleanFields();
+
+    newProduct = document.getElementById("list");
+    newProduct.classList.remove("style-list");
+    newProduct.innerHTML += `
+    <div class="style-list2">
+    <h1>${productName}</h1>
+    <p>Preço: ${price} $</p>
+    <p>Categoria: ${category}</p>
+    <div id="btn-edit-remove">
+    <button type="button" onclick="edit" class="editButton"><i class="fa-solid fa-pencil" style="color: #ffffff;"></i></button>
+    <button type="button" onclick="remove" class="removeButton"><i class="fa-solid fa-trash" style="color: #ffffff;"></i></button>
+    </div>
+    </div>
+    <ul class="productsListByCategory">`;
+
+    console.log(productList.products);
+}
+
+function remove() {
+    let removeProduct = document.getElementById("list");
+    
+}
+
+function cleanFields() {
+    document.getElementById("name-product").value = "";
+    document.getElementById("price-product").value = "";
 }
