8d8c9c1e92f528eaa2b342bc3721d555676596ab
João Pedro Rocha <jpcrocha8@gmail.com>
2023-08-17 11:14:51 -0300

    feat: create addProduct

--- criacao-de-categorias/script.js
+++ criacao-de-categorias/script.js
@@ -23,13 +23,31 @@ class CategoryService {
 
     addCategory(name) {
         const id = this.nextCategoryID ++;
-        const category = new Category('id', 'name');
+        const category = new Category(id, name);
+        this.categories.push(category);
     }
 }
 
+class ProductServices {
+    constructor() {
+        this.products = [];
+        this.nextProductId = 0;
+    }
 
+    addProduct(name, price, category) {
+        const id = this.nextProductId ++;
+        const product = new Product(id, name, price, category);
+        category.product.push(product);
+        this.products.push(product);
+    }
+}
 
+const categoryList = new CategoryService();
 
+function createCategory() {
+    const categoryName = "Doce";
 
+    categoryList.addCategory(categoryName);
 
-
+    console.log(categoryList.categories);
+}
