bdc161a0f531cb9a9955b190d49436903bfac853
João Pedro Rocha <jpcrocha8@gmail.com>
2023-09-05 17:14:08 -0300

    feat: finalization js

--- agenda-telefônica/assets/js/script.js
+++ agenda-telefônica/assets/js/script.js
@@ -6,19 +6,19 @@ class User {
         this.urlImage = urlImage;
         this.birthday = birthday;
         this.sign = this.getZodiacSign(this.birthday);
-        this.age = getAge();
+        this.age = this.getAge(this.birthday);
         this.email = email;
         this.cep = cep;
         this.city = city;
         this.instagram = instagram;
         this.github = github;
+        this.id = (Math.random() * 9999);
     }
 
-    getZodiacSign() {
-        let birthdate = new Date(this.birthayDate);
+    getZodiacSign(birth) {
+        let birthdate = new Date(birth);
         let day = birthdate.getDate();
         let month = birthdate.getMonth() + 1;
-        console.log("Passou pelo getSigno() da class User");
 
         if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
             return "Capricórnio ♑";
@@ -47,29 +47,18 @@ class User {
         }
     }
 
-    getAge() {
-        let nowDate = new Date();
-        let nowYear = nowDate.getFullYear();
-        let nowMonth = nowDate.getMonth();
-        let birthdate = new Date(this.birthday);
-        let year = birthdate.getFullYear();
-        let month = birthdate.getMonth();
-        let day = birthdate.getDay();
+    getAge(birth) {
+        let today = new Date();
+        let birthdate = new Date(birth);
+        let age = today.getFullYear() - birthdate.getFullYear();
+        let month = today.getMonth() - birthdate.getMonth();
 
-        console.log(birthdate);
-        console.log(month);
-        console.log(day);
-
-        let counter = nowYear - year;
-
-        if (nowMonth < month || nowMonth == month && nowDate < day) {
+        if (month < 0 || (month === 0 && today.getDate() < birthdate.getDate())) {
+            age--;
         }
-
-        return counter < 0 ? 0 : counter;
+        return age;
     }
 }
-
-const links = new User();
 
 class ListUser {
     constructor() {
@@ -77,55 +66,44 @@ class ListUser {
     }
 
     addUser(user) {
-        this.users.push(user);
-    }
-
-    createContact() {
-        if (isAnyInputEmpty()) {
-            sendErrorMsg("Preencha os campos vazios");
-        }else if (verifyUrlImage()) {
-            sendErrorMsg("Imagem não encontrada!")
-        }else if(verifyUrlSocialNetwork(links.instagram)) {
-            sendErrorMsg("Não é uma URL válida do Instagram.");
-        }else if(verifyUrlSocialNetwork(links.github)){
-            sendErrorMsg("Não é uma URL válida do GitHub.");
+        if (isAnyInputEmpty(user)) {
+            sendErrorMsg("Preencha todos campos!");
+        }else if(verifyUrlImage(user.urlImage)) {
+            sendErrorMsg("link da imagem inválido");
         }else {
-            sendSucessMsg('Contato adicionado a biblioteca');
-            LibaryUsers.addUser(this.users);
+            sendSucessMsg('Contato adicionado à biblioteca');
+            this.users.push(user);
             clearInputs();
-
-            let showCardUser = document.getElementById("container2");
-            showCardUser.innerHTML = '';
-
-            LibaryUsers.users.forEach(user => {
-                showCardUser.innerHTML += `
-            <div class="contact" onclick="showInformation()">
-            <img class="style-img" id="img" src="${user.urlImage}">
-            <p id="style-name">${user.name}</p>
-            <p id="style-telFix">${formatedCellphone(user.telFix)}</p>
-            <p id="style-telCell">${formatedCellphone(user.telCell)}</p>
-            </div>`
-            });
+            createUser();
         }
     }
+
+    VerifyIdExist(id){
+        return this.users.find(user => user.id === id)
+    }
 }
 
-const LibaryUsers = new ListUser();
+const libaryUsers = new ListUser();
 
-function formatedTelFix(cellphone) {
-    console.log("Passou pela funcao formatedCellphone()");
+function createContact() {
+    const name = document.getElementById("name").value;
+    const telFix = document.getElementById("telFix").value;
+    const telCell = document.getElementById("telCell").value;
+    const urlImage = document.getElementById("urlImage").value;
+    const birthday = document.getElementById("birthday").value;
+    const email = document.getElementById("email").value;
+    const cep = document.getElementById("cep").value;
+    const city = document.getElementById("city").value;
+    const instagram = document.getElementById("instagram").value;
+    const github = document.getElementById("github").value;
 
-    let cellphoneArray = cellphone.split("");
-    let cellphoneFormated = "(" + cellphoneArray[0] + cellphoneArray[1] + ")"
-        + " " + cellphoneArray[2] + cellphoneArray[3] + cellphoneArray[4]
-        + cellphoneArray[5] + cellphoneArray[6] + "-"
-        + cellphoneArray[7] + cellphoneArray[8]
-        + cellphoneArray[9] + cellphoneArray[10];
-    return cellphoneFormated;
+    const user = new User(name, telFix, telCell, urlImage, birthday, email, cep, city, instagram, github);
+
+    libaryUsers.addUser(user);
 }
+
 
 function sendErrorMsg(msg) {
-    console.log("Passou pela funcao sendErrorMsg()");
 
     document.getElementById("error-msg").innerHTML = msg;
     document.getElementById("error-msg").classList.remove("hidden");
@@ -181,43 +159,82 @@ function clearInputs() {
 }
 
 function verifyUrlImage(url) {
-
-    if (url.match(/\.(Jpeg|jpg|gif|png|webp)$/) != null) {
-        return true;
+    if (url.match(/\.(jpeg|jpg|gif|png|webp)$/i) != null) {
+        return false;
     } else {
-        return false;
-    }
-
-}
-
-function verifyUrlSocialNetwork(url) {
-    if(url == "https://github.com/" + github || url == "https://www.instagram.com/" + instagram) {
         return true;
-    }else {
-        return false;
     }
 }
 
-function showInformation() {
+function formatedCellphone(cellphone) {
+
+    let cellphoneArray = cellphone.split("");
+    let cellphoneFormated = "(" + cellphoneArray[0] + cellphoneArray[1] + ")"
+        + " " + cellphoneArray[2] + cellphoneArray[3] + cellphoneArray[4]
+        + cellphoneArray[5] + cellphoneArray[6] + "-"
+        + cellphoneArray[7] + cellphoneArray[8]
+        + cellphoneArray[9] + cellphoneArray[10];
+    return cellphoneFormated;
+}
+
+function formatedCEP(value) {
+    let cep = String(value).split("");
+    let cepNew = cep[0] + cep[1] + cep[2] + cep[3] + cep[4] + "-" + cep[5] + cep[6] + cep[7];
+
+    return cepNew;
+}
+
+function dateBr(date) {
+    const dateArray = date.split("-");
+    const datePTBR = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+    return datePTBR;
+}
+
+function createUser() {
+    let showCardUser = document.getElementById("container2");
+    showCardUser.innerHTML = '';
+
+        libaryUsers.users.forEach(user => {
+        showCardUser.innerHTML += `
+        <div class="carContact" onclick="showInformation(${user.id})">
+        <img src="${user.urlImage} alt="${user.name}-photo">
+        <div class="information-card">
+        <p>${user.name}</p>
+        <p>Telefone fixo: ${formatedCellphone(user.telFix)}</p>
+        <p>Telefone Celular: ${formatedCellphone(user.telCell)}</p>
+        </div>
+        </div>`
+    });
+}
+
+function showInformation(id) {
+    const user = libaryUsers.VerifyIdExist(id);
     let showUser = document.getElementById("container3");
     showUser.innerHTML = '';
 
-    LibaryUsers.users.forEach(user => {
         showUser.innerHTML += `
+        <h1>Detalhes</h1>
+        <main class="big-card">
         <img class="style-img" id="img" src="${user.urlImage}">
         <p id="style-name">${user.name}</p>
         <p id="style-id">${user.id}</p>
-        <p id="style-telCell">${user.telCell}</p>
-        <p id="style-telFix">${user.telFix}</p>
-        <p id="style-birthday">${user.birthdate}</p>
+        </div>
+        <div class="big-card2">
+        <p id="style-telCell">${formatedCellphone(user.telCell)}</p>
+        <p id="style-telFix">${formatedCellphone(user.telFix)}</p>
+        <p id="style-birthday">${dateBr(user.birthday)}</p>
         <p id="style-age">${user.age}</p>
         <p id="style-sign">${user.sign}</p>
         <p id="style-email">${user.email}</p>
-        <p id="style-cep">${user.cep}</p>
+        <p id="style-cep">${formatedCEP(user.cep)}</p>
         <p id="style-city">${user.city}</p>
         <p id="style-instagram">${user.instagram}</p>
         <p id="style-github">${user.github}</p>
-        <button type="button" id="btn-edit" class="btnEdit" onclick="edit"><i class="fa-solid fa-pencil" style="color: #000000;"></i></button>
-        <button type="button" id="btn-remove" class="btnRemove" onclick="remove"><i class="fa-solid fa-pencil" style="color: #000000;"></i></button>`
-    });
+        </div>
+        <div class="socialMedias">
+        <a href="https://wa.me/55${user.telCell}"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="https://www.instagram.com/${user.instagram}/"><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://github.com/${user.github}"><i class="fa-brands fa-github"></i></a>
+        </div>
+        </main>`
 }
